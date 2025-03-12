79381c20b19ee3558a9b4fd38eb127578bddcc9b
Acktar Badulla <itsmeacktaryt@gmail.com>
2025-03-12 10:41:03 +0400

    Update AddMoneyCommand.java

--- src/main/java/com/acktar/economyapi/commands/AddMoneyCommand.java
+++ src/main/java/com/acktar/economyapi/commands/AddMoneyCommand.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 public class AddMoneyCommand extends SimpleCommand {
     public AddMoneyCommand() {
         super("addmoney", "Add money to a player's balance!");
+        aliases.add("am");
     }
 
 @Override
